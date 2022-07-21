fb5400d7af8d9bfb15b4bcaa983456bae0131df6
ranhengzhang <2668394257@qq.com>
2022-07-22 02:35:39 +0800

    vue 072

---
+++ vue/vue脚手架/demo/todo_list/src/components/MyList.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="todo in todos" :key="todo.id" :todo="todo"/>
+  </ul>
+</template>
+
+<script>
+import MyItem from "@/components/MyItem";
+
+export default {
+  name: "MyList",
+  components: {MyItem},
+  data() {
+    return {
+      todos: [
+        {
+          id: '001',
+          title: "吃饭",
+          done: true
+        },
+        {
+          id: '002',
+          title: "睡觉",
+          done: false
+        },
+        {
+          id: '003',
+          title: "循环",
+          done: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
